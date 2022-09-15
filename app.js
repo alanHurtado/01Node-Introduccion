afad845241c42f3de45dff15a242dac69f79bbc2
Alan Astroga Hurtado <alanasto10@gmail.com>
2022-09-15 12:52:09 -0500

    fin Lista de tareas

--- app.js
+++ app.js
@@ -1,7 +1,81 @@
-const { argv } = require("./bases-node/config/yargs");
-const { tabla: crearArchivo } = require("./bases-node/helpers/multiplicar");
+require('colors');
+
+const { guardarDB, leerDB } = require('./app-consola-tareas/helpers/guardarArchivo');
+const { inquirerMenu, 
+        pausa,
+        leerInput,
+        listadoTareasBorrar,
+        confirmar,
+        mostrarListadoChecklist
+} = require('./app-consola-tareas/helpers/inquirer');
+
+const Tareas = require('./app-consola-tareas/models/tareas');
 
 
-console.clear();
+const main = async() => {
 
-crearArchivo(argv.b, argv.l);
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if ( tareasDB ) { // cargar tareas
+        tareas.cargarTareasFromArray( tareasDB );
+    }
+
+    do {
+        // Imprimir el menú
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                // crear opcion
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea( desc );
+            break;
+
+            case '2':
+                tareas.listadoCompleto();
+            break;
+            
+            case '3': // listar completadas
+                tareas.listarPendientesCompletadas(true);
+            break;
+
+            case '4': // listar pendientes
+                tareas.listarPendientesCompletadas(false);
+            break;
+
+            case '5': // completado | pendiente
+                const ids = await mostrarListadoChecklist( tareas.listadoArr );
+                tareas.toggleCompletadas( ids );
+            break;
+                       
+            case '6': // Borrar
+                const id = await listadoTareasBorrar( tareas.listadoArr );
+                if ( id !== '0' ) {
+                    const ok = await confirmar('¿Está seguro?');
+                    if ( ok ) {
+                        tareas.borrarTarea( id );
+                        console.log('Tarea borrada');
+                    }
+                }
+            break;
+        
+        }
+
+
+        guardarDB( tareas.listadoArr );
+
+        await pausa();
+
+    } while( opt !== '0' );
+
+
+    // pausa();
+
+}
+
+
+main();
+
